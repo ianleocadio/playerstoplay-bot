152017be8aebdc56e54ce3d3cc757bb1658f4598
Ian Leocádio <ian.leocadio@techne.com.br>
2018-01-31 16:09:17 -0200

    Add files via upload

---
+++ commands/commands.js
@@ -0,0 +1,3 @@
+module.exports = {
+	"Help": require("./help.js")
+}
---
+++ commands/help.js
@@ -0,0 +1,49 @@
+function listHelp(message){
+	message.author.sendMessage(helpText);
+}
+
+var helpText =
+  "```Markdown\n"
++ "# Lista de comandos:\n\n"
++ "[1]: Adicionar membro ao cargo Player\n"
++ "	   /add '@menções' (Ex: /add @Fulano @Pipitchu)\n\n"
+// + "[2]:"
++ "```";
+
+
+
+
+function possibleCommand(message){
+	var cantExecuteHelpCommand = false;
+	message.channel.fetchMessages({limit : 5, before: message.id})
+		.then(messages => {
+			const alreadyCommands = ["/gamerescape", "/xivdb", "/giphy", "/tenor", "/tts", "/me", "/tableflip", "/unflip", "/shrug"];
+			var noneOfThen = true;
+			alreadyCommands.map(function(command){
+				if(message.content.startsWith(command))
+					noneOfThen = false;
+			});
+			console.log(noneOfThen);
+			messages.map(function(bm){
+				if(bm.content.startsWith("/") && noneOfThen && (Date.now() - bm.createdTimestamp < 300000))
+					cantExecuteHelpCommand = true;
+			});
+
+			if (cantExecuteHelpCommand){
+				message.delete();
+				return;
+			}
+			
+
+			
+			if (noneOfThen)
+				message.reply("`Possível tentativa de usar um commando avistado! Utilize '/help' para listar os comandos atuais`  :upside_down:");
+		});
+	
+}
+
+
+module.exports = {
+	"listHelp": listHelp,
+	"possibleCommand": possibleCommand
+}
