360f978f24186901b14c56f6c366fed8f77028af
Ian Leocádio <ian.leocadio@techne.com.br>
2018-01-31 16:07:21 -0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const auth = require("./auth.json")
+
+const Commands = require('./commands/commands.js');
+
+var bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", () => {
+	console.log("BOT Online");
+});
+
+bot.on("message", message => {
+	
+
+	if(message.content.startsWith("/")){
+		if(message.content.startsWith("/help")){
+			Commands.Help.listHelp(message);
+			return;
+		}
+
+		//Comandos...
+
+
+		//Final
+		Commands.Help.possibleCommand(message);
+
+	}
+
+});
+
+bot.login(auth.token);
+
