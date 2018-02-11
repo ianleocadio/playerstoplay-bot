8363225b4991e40ca2bd93bb1d010a1e5dde4c87
Ian Leocádio <ian.c.leocadio@gmail.com>
2018-02-11 19:03:38 -0200

    Correções auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -1,30 +1,37 @@
 var auth;
-function authToken(){
+function authToken(c){
 	try{
 		auth = require('./auth.json');
 
-		let map = new Map();
-		auth.STAFF.map(function(s){
-		    map.set(s.id, s.name);
-        });
-		auth.STAFF = map;
+		c(auth.STAFF, auth.MEMBERS);
 
-		map = new Map();
-		auth.MEMBERS.map(function(m){
-		    map.set(m.id, m.name);
-        });
-		auth.MEMBERS = map;
 	}catch(e){
 		auth = {
 			"token": process.env.BOT_TOKEN,
 			"yt_key": process.env.YT_KEY,
-
-			"STAFF": (process.env.STAFF || []),
-            "MEMBERS": (process.env.MEMBERS || [])
+			"STAFF": process.env.STAFF,
+			"MEMBERS": process.env.MEMBERS
 		}
+
+		c(auth.STAFF, auth.MEMBERS);
+
 	}
 }
 
-authToken();
+authToken(function(STAFF, MEMBERS){
+	let map = new Map();
+	STAFF.map(function(s){
+	    map.set(s.id, s.name);
+    });
+	auth.STAFF = map;
+
+	map = new Map();
+	MEMBERS.map(function(m){
+	    map.set(m.id, m.name);
+    });
+	auth.MEMBERS = map;
+});
+
+console.log(auth);
 
 module.exports = auth
