65f9c1e38c43917c83ac270d59f26e47431c0227
Ian Leocádio <ian.c.leocadio@gmail.com>
2019-02-02 15:51:20 -0200

    add

--- commands/warframe/embeds/StatusAlertWatcherEmbed.js
+++ commands/warframe/embeds/StatusAlertWatcherEmbed.js
@@ -1,6 +1,6 @@
 const AlertEmbed = require("./AlertEmbed.js");
-const BaseEmbed = require('./BaseEmbed.js');
-const assetBase = 'https://cdn.warframestat.us/genesis';
+const BaseEmbed = require("./BaseEmbed.js");
+const assetBase = "https://cdn.warframestat.us/genesis";
 const Utils = require("../../../util/utils");
 
 const alertThumb = `${assetBase}/img/alert.png`;
@@ -18,19 +18,19 @@ class StatusAlertWatcherEmbed extends BaseEmbed {
     show() {
         this.thumbnail = {
             url: `${assetBase}/img/outage.png`
-        }
+        };
 
         this.color = 0xfa727b;
         this.title = "Alert Watcher";
-        this.fields.push({ name: `Category`, value: `${this.item.id}`, inline: true });
+        this.fields.push({ name: "Category", value: `${this.item.id}`, inline: true });
         if (this.item.isRunning) {
             if (this.item.found) {
-                this.fields.push({ name: `Found ?`, value: "Yes", inline: true });
+                this.fields.push({ name: "Found ?", value: "Yes", inline: true });
                 let date = Utils.formatDate(this.item.alerts[0].expiry);
                 date = `${date.dayString}, ${date.day} ${date.monthString} ${date.year} at ${date.hours}:${date.minutes}`;
-                this.fields.push({ name: `Status:`, value: `Will started again on ${date}`, inline: true });
+                this.fields.push({ name: "Status:", value: `Will started again on ${date}`, inline: true });
                 
-                this.fields.push({ name: `Current Worldstate - Alerts`, value: ``, inline: false });
+                this.fields.push({ name: "Current Worldstate - Alerts", value: "", inline: false });
                 let activeAlerts = (new AlertEmbed(this.item.alerts, "PC")).showStatus();
                 if (activeAlerts.length > 0){
                     activeAlerts.map(aa=>{
@@ -39,20 +39,20 @@ class StatusAlertWatcherEmbed extends BaseEmbed {
                 }
 
             } else {
-                this.fields.push({ name: `Found ?`, value: "No", inline: true });
-                this.fields.push({ name: `Status:`, value: "Waiting for the alert appears", inline: true });
+                this.fields.push({ name: "Found ?", value: "No", inline: true });
+                this.fields.push({ name: "Status", value: "Waiting for the alert appears", inline: true });
             }
         } else {
             if (this.item.found) {
-                this.fields.push({ name: `Found ?`, value: "Yes", inline: true });
-                this.fields.push({ name: `Status`, value: "Stopped", inline: true });
+                this.fields.push({ name: "Found ?", value: "Yes", inline: true });
+                this.fields.push({ name: "Status", value: "Stopped", inline: true });
             } else {
-                this.fields.push({ name: `Found ?`, value: "No", inline: true });
-                this.fields.push({ name: `Status`, value: "Stopped", inline: true });
+                this.fields.push({ name: "Found ?", value: "No", inline: true });
+                this.fields.push({ name: "Status", value: "Stopped", inline: true });
             }
         }
 
-        this.footer.text = `Created at `;
+        this.footer.text = "Created at ";
         this.timestamp = this.item.createdAt;
 
 
@@ -74,15 +74,15 @@ class StatusAlertWatcherEmbed extends BaseEmbed {
      
            this.fields = [];
            if (this.description !== summary) {
-             this.fields.push({ name: 'Mission', value: `${a.mission.faction} ${a.mission.type}`, inline: true });
-             this.fields.push({ name: 'Location', value: a.mission.node, inline: true });
+             this.fields.push({ name: "Mission", value: `${a.mission.faction} ${a.mission.type}`, inline: true });
+             this.fields.push({ name: "Location", value: a.mission.node, inline: true });
            }
-           this.fields.push({ name: 'Levels:', value: `${a.mission.minEnemyLevel} - ${a.mission.maxEnemyLevel}`, inline: true });
+           this.fields.push({ name: "Levels:", value: `${a.mission.minEnemyLevel} - ${a.mission.maxEnemyLevel}`, inline: true });
      
-           this.fields.push({ name: 'Archwing Required', value: a.mission.archwingRequired ? 'Yes' : 'No', inline: true });
+           this.fields.push({ name: "Archwing Required", value: a.mission.archwingRequired ? "Yes" : "No", inline: true });
      
-           if (this.title.indexOf('Cr') === -1) {
-             this.fields.push({ name: '\u200B', value: `**Credits:** ${a.mission.reward.credits}`, inline: true });
+           if (this.title.indexOf("Cr") === -1) {
+             this.fields.push({ name: "\u200B", value: `**Credits:** ${a.mission.reward.credits}`, inline: true });
            }
            this.footer.text = `${a.eta} remaining • Expires `;
            this.timestamp = a.expiry;
