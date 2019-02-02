0b12e3ac6bffbe45ece8c657c1b367cca69303d7
Ian Leocádio <ian.c.leocadio@gmail.com>
2019-02-02 16:07:21 -0200

    add

--- config/config.js
+++ config/config.js
@@ -1,25 +1,7 @@
 const Discord = require("discord.js");
 let BOT_CONFIG = require("./bot_config.js");
-
 const Utils = require("../util/utils.js");
-
-function commands(message){
-	let commandArguments = message.content.match(/\S+/g);
-	let command = (commandArguments[1] || "").toLowerCase();
-	
-	if(!commandArguments[1]){
-	    ConfigPermissions.call(message, showConfigInfo, message);
-	}else
-
-	if(command === "textchannel" || command === "tc"){
-	    ConfigPermissions.call(message, setTextChannel, message, commandArguments);
-	}else
-
-	if(command === "voicechannel" || command === "vc"){
-	    ConfigPermissions.call(message, setVoiceChannel, message, commandArguments);
-	}
-
-}
+const CommandMap = require("./CommandMap");
 
 /******************************************************
  *													  *
@@ -28,77 +10,79 @@ function commands(message){
  *													  *
  *													  *
  ******************************************************/
-function showConfigInfo(message){
-	let embed = new Discord.RichEmbed();
-	embed.setAuthor("Configurações atuais:")
-		 .setColor("RED");
-	let textChannelStr = "\n";
-	BOT_CONFIG.textChannels.map(function(tc){
-	    textChannelStr += "**"+tc.toString()+"**\n";
+function showConfigInfo(message) {
+    let embed = new Discord.RichEmbed();
+    embed.setAuthor("Configurações atuais:")
+        .setColor("RED");
+    let textChannelStr = "\n";
+    BOT_CONFIG.textChannels.map(function (tc) {
+        textChannelStr += "**" + tc.toString() + "**\n";
     });
-	let voiceChannelStr = "\n";
-	BOT_CONFIG.voiceChannels.map(function(vc){
-        voiceChannelStr += "**"+vc.toString()+"**\n";
+    let voiceChannelStr = "\n";
+    BOT_CONFIG.voiceChannels.map(function (vc) {
+        voiceChannelStr += "**" + vc.toString() + "**\n";
     });
 
     embed.addField("Chats padrões", (BOT_CONFIG.textChannels.length > 0) ? textChannelStr : "Não definido", true)
-		 .addField("Canais de voz padrão", (BOT_CONFIG.voiceChannels > 0) ? voiceChannelStr : "Não definido", true);
+        .addField("Canais de voz padrão", (BOT_CONFIG.voiceChannels > 0) ? voiceChannelStr : "Não definido", true);
 
-	message.channel.send(embed);
+    message.channel.send(embed);
 }
 
-function configInfo(){
+function configInfo() {
     return new Discord.RichEmbed()
         .setColor("RED")
-        .setAuthor("Configuração alterada:")
+        .setAuthor("Configuração alterada:");
 }
 
-function setTextChannel(message, commandArguments){
-	let textChannels = Array.prototype.slice.call(commandArguments, 2);
+function setTextChannel(message, commandArguments) {
+    let textChannels = Array.prototype.slice.call(commandArguments, 2);
 
-    for(i=0; i < textChannels.length; i++){
-        let textChannel = message.guild.channels.find(function(tc){
+    for (let i = 0; i < textChannels.length; i++) {
+        let textChannel = message.guild.channels.find((tc) => {
             return (tc.name === textChannels[i]) && (tc.type === "text");
         });
-        if (textChannel){
-            if(!BOT_CONFIG.textChannels.includes(textChannel)){
+        if (textChannel) {
+            if (!BOT_CONFIG.textChannels.includes(textChannel)) {
                 BOT_CONFIG.textChannels.push(textChannel);
-            }else
+            } else{
                 return;
+            }
         }
     }
     let embed = configInfo();
     let canais = "\n";
-    BOT_CONFIG.textChannels.map(function(tc){
-        canais += "**"+tc.toString()+"**\n";
+    BOT_CONFIG.textChannels.map((tc) => {
+        canais += "**" + tc.toString() + "**\n";
     });
-    embed.addField("Chat padrão", "Comandos do bot só poderão ser realizados nos chats: "+canais, true)
+    embed.addField("Chat padrão", "Comandos do bot só poderão ser realizados nos chats: " + canais, true)
         .addField("Modificado por", message.author.toString(), false);
     message.channel.send(embed);
 
 }
 
-function setVoiceChannel(message, commandArguments){
+function setVoiceChannel(message, commandArguments) {
     let voiceChannels = Array.prototype.slice.call(commandArguments, 2);
 
-    for(i=0; i < voiceChannels.length; i++){
-        let voiceChannel = message.guild.channels.find(function(vc){
+    for (let i = 0; i < voiceChannels.length; i++) {
+        let voiceChannel = message.guild.channels.find((vc) => {
             return (vc.name === voiceChannels[i]) && (vc.type === "voice");
         });
-        if (voiceChannel){
-            console.log(voiceChannel.name +" - "+ voiceChannel.type);
-            if(!BOT_CONFIG.voiceChannels.includes(voiceChannel)){
+        if (voiceChannel) {
+            console.log(voiceChannel.name + " - " + voiceChannel.type);
+            if (!BOT_CONFIG.voiceChannels.includes(voiceChannel)) {
                 BOT_CONFIG.voiceChannels.push(voiceChannel);
-            }else
+            } else{
                 return;
+            }
         }
     }
     let embed = configInfo();
     let canais = "\n";
-    BOT_CONFIG.voiceChannels.map(function(vc){
-        canais += "**"+vc.toString()+"**\n";
+    BOT_CONFIG.voiceChannels.map((vc) => {
+        canais += "**" + vc.toString() + "**\n";
     });
-    embed.addField("Chat padrão", "O bot só poderá entrar nos canais: "+canais, true)
+    embed.addField("Chat padrão", "O bot só poderá entrar nos canais: " + canais, true)
         .addField("Modificado por", message.author.toString(), false);
     message.channel.send(embed);
 }
@@ -120,12 +104,43 @@ let ConfigPermissions = new Permissions(Utils.createListOfPermissions(functions)
 /******************************************************
  *													  *
  *													  *
+ *						Command					      *
+ *													  *
+ *													  *
+ ******************************************************/
+let commandsList = new CommandMap();
+commandsList.set(/^(?:tc|textchannel)$/g, setTextChannel);
+commandsList.set(/^(?:vc|voicechannel)$/g, setVoiceChannel);
+
+
+
+function commands(message) {
+    let commandArguments = message.content.match(/\S+/g);
+    let command = (commandArguments[1] || "").toLowerCase();
+
+    if (!command) {
+        ConfigPermissions.call(message, showConfigInfo, message);
+        return;
+    } else {
+
+        command = commandsList.getCommandImplementation(command);
+        if (command) {
+            ConfigPermissions.call(message, command, message, commandArguments);
+        } else {
+            ConfigPermissions.call(message, showConfigInfo, message);
+            return;
+        }
+    }
+
+}
+
+
+
+/******************************************************
+ *													  *
+ *													  *
  *						Exports 					  *
  *													  *
  *													  *
  ******************************************************/
-module.exports = {
-	"commands": commands
-}
-
-
+module.exports = { commands };
